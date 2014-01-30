2d4d71ff198fdfa2c45742eed305dce3eb8f400c
Jake Valletta (jakev) <javallet@gmail.com>
2014-01-30 18:09:00 -0500

    Added Seclog API

--- core/java/android/util/Log.java
+++ core/java/android/util/Log.java
@@ -336,6 +336,7 @@ public final class Log {
     /** @hide */ public static final int LOG_ID_RADIO = 1;
     /** @hide */ public static final int LOG_ID_EVENTS = 2;
     /** @hide */ public static final int LOG_ID_SYSTEM = 3;
+    /** @hide */ public static final int LOG_ID_SECURITY = 4;
 
     /** @hide */ public static native int println_native(int bufID,
             int priority, String tag, String msg);
